6345a1f4ca10883e017dbf63f91049760fd35dd5
Cameron Cooks <cameronc56@ymail.com>
2017-12-21 16:56:27 -0800

    removing useless files

--- spec/features/extension_will_load_in_feature_tests.js
+++
@@ -1,7 +0,0 @@
-describe('chrome extension is loaded properly when feature tests run', function () {
-  it('loads an alert on the page', function () {
-    browser.url('https://www.google.com')
-    browser.pause(1000)
-    expect(browser.alertText()).toEqual("Hello. This message was sent from scripts/inject.js")
-  })
-})
